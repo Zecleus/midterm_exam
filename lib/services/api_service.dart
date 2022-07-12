c4cd1f57e8b86659aedfcc84acf7290496f343a7
Kenneth Cuico <kenneth.cuico123@gmail.com>
2022-07-12 10:46:34 +0800

    Added login in service and fixed login_screen

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,21 @@
+import 'dart:convert';
+
+import 'package:fake_store/models/api_response.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+
+  Future<dynamic> login(String userN, String pass) {
+    return http.post(Uri.parse(baseUrl + "/auth/login"),
+        body: {'username': userN, 'password': pass}).then((data) {
+      if (data.statusCode == 201) {
+        final jsonData = json.decode(data.body);
+
+        return Future<dynamic>(jsonData);
+      }
+      return APIResponse<String>(error: true, errorMessage: 'An error occured');
+    }).catchError((_) =>
+        APIResponse<String>(error: true, errorMessage: 'An error occured'));
+  }
 }
