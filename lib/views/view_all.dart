c2f721e884c55ce49e7b0ccbc1fddc3316b1067d
Kenneth Cuico <kenneth.cuico123@gmail.com>
2022-07-12 08:44:14 +0800

    initial commit

---
+++ lib/views/view_all.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ViewAll extends StatefulWidget {
+  const ViewAll({Key? key}) : super(key: key);
+
+  @override
+  State<ViewAll> createState() => _ViewAllState();
+}
+
+class _ViewAllState extends State<ViewAll> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
