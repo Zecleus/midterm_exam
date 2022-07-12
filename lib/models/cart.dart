ca1a1d5bb8139f8e53a2cfedadce730212745d17
Kenneth Cuico <kenneth.cuico123@gmail.com>
2022-07-12 13:02:30 +0800

    Created dart model and json serializable
    also did some edits in home
    getSingleProduct alread implemented in homeScreen branch

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,11 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'cart.g.dart';
+
+@JsonSerializable()
 class Cart {
   int? id;
   int? userId;
   DateTime? date;
-  dynamic products = {
-    'productId': int,
-    'quantity': int,
-  };
+  List<dynamic>? products;
 
   Cart({this.id, this.userId, this.date, this.products});
+
+  factory Cart.fromJson(Map<String, dynamic> item) => _$CartFromJson(item);
 }
